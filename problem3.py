22925ec42d40cccbd0b99d43843a33fe12dfbe65
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-21 17:38:30 -0500

    Add function to check if number is prime

--- problem3.py
+++ problem3.py
@@ -2,3 +2,12 @@
 
 '''What is the largest prime factor of the number 600851475143 ?'''
 
+number = 600851475143
+
+def is_prime(number):
+	if number in [0,1]:
+		return None
+	for i in range(2,number):
+		if number % i == 0:
+			return False
+	return True
