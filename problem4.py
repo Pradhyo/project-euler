2bd09b6aaad151481ac4fc7d8218938d68d260d6
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-22 19:33:28 -0500

    Add function to check if number is palindrome

--- problem4.py
+++ problem4.py
@@ -1,3 +1,6 @@
 # Problem 2 from project Euler
 
 '''Find the largest palindrome made from the product of two 3-digit numbers'''
+
+def is_palindrome(number):
+	return str(number) == str(number)[::-1]
