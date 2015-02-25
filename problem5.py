5c7e7724c9fa30badf933b4395f70117f05d2864
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-25 00:35:18 -0500

    Add function to return the factors of given number

--- problem5.py
+++ problem5.py
@@ -1,4 +1,20 @@
-# Problem 1 from project Euler
+# Problem 5 from project Euler
 
 ''' What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20? '''
+
+number = 20
+factorsofanswer = {}
+
+def factor(number):
+	"""Given a number, return its factors as a dictionary """
+	factors = {}
+	for i in range(2,(number+2)/2):
+		while number % i == 0:
+			factors[i] = factors.get(i,0) + 1
+			number /= i
+	return factors
+
+
+
+
