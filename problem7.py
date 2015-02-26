915d0ea0207c6d2774c0f0652cbd07fac5d27887
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-26 01:00:41 -0500

    Add function to find if a number is prime

--- problem7.py
+++ problem7.py
@@ -1,3 +1,11 @@
 # Problem 7 from project Euler
 
 '''What is the 10 001st prime number?'''
+
+def is_prime(number):
+	if number == 0 or number == 1:
+		return None
+	for i in range(2,int(number**0.5)+1):
+		if number % i == 0:
+			return False
+	return True
