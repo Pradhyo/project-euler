a060f5f508000e1fc0e06867d9d9d511919ffa8c
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-01 19:19:18 -0500

    Reduce range of b and split into multiple lines

--- problem9.py
+++ problem9.py
@@ -5,6 +5,8 @@ Find the product abc (a<b<c). '''
 
 number = 1000
 
-answer = [a*b*(number-(a+b)) for a in range(1,number/3) for b in range(a+1,1000) if a*a + b*b == (number-a-b)**2]
-
+answer = [a*b*(number-(a+b)) for a in range(1,number/3) 
+							 for b in range(a+1,(number-a)/2) 
+							 if a*a + b*b == (number-a-b)**2]
+							 
 print answer
