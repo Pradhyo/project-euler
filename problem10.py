db44386b9c0a0c2394be1a26ff56301ba5d860da
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-02 15:02:34 -0500

    Add code to check if a number is prime

---
+++ problem10.py
@@ -0,0 +1,11 @@
+# Add problem 10 from project Euler
+
+''' Find the sum of all the primes below two million. '''
+
+def is_prime(number):
+	if number in [0,1]:
+		return None
+	for i in range(2,int(number**0.5)+1):
+		if number % i == 0:
+			return False
+	return True
