16935128f7f73a1d93cffc21b062364813964195
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-04 01:26:10 -0500

    Improve is_prime function

--- problem10.py
+++ problem10.py
@@ -5,8 +5,12 @@
 def is_prime(number):
 	if number in [0,1]:
 		return None
-	for i in range(2,int(number**0.5)+1):
-		if number % i == 0:
+	if number in [2,3]:
+		return True
+	if number % 2 == 0 or number % 3 == 0:
+		return False
+	for i in range(5,int(number**0.5)+1,6):
+		if number % i == 0 or number % (i+2) == 0:
 			return False
 	return True
 
