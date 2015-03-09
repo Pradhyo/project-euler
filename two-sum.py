a07cad7ffc0fb1eede79746dd4c54e515ef4c820
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-09 01:19:34 -0400

    Solve using hash

--- two-sum.py
+++ two-sum.py
@@ -11,7 +11,26 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 """
 
+
 class Solution:
-    # @return a tuple, (index1, index2)
-    def twoSum(self, num, target):
-        
+    # return a tuple, (index1, index2) whos values add up to target
+
+    def twoSum(self,num,target):
+    	if target % 2 == 0 and num.count(target/2) > 1:
+    		index1 = num.index(target/2)+1
+    		num = num[index1:]
+    		return index1, num.index(target/2)
+    	num_hash = {}
+    	for i in range(len(num)):
+    		num_hash[num[i]] = i
+    	for i in range(len(num)):
+    		if target - num[i] in num_hash and target/2 != num[i]:
+    			return i+1, num_hash[target-num[i]]+1
+    	
+
+input1 = Solution()
+print input1.twoSum([3,2,4,0,0,1,1], target=2)
+
+
+
+
